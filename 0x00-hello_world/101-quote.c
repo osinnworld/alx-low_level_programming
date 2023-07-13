bd8b763b707a0addf5ce861fa3fddf84f436f4f3
osinnworld <shemogata@gmail.com>
2023-07-13 12:19:41 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main(void)
-{
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    const unsigned int message_length = sizeof(message) - 1;
+int main(void) {
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = sizeof(message) - 1;
+    ssize_t bytes_written = write(2, message, len);
 
-    if (fprintf(stderr, "%s", message) < message_length) {
+    if (bytes_written != len) {
         return 1;
     }
 
