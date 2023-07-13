f355f648c3bf6801ec87989744facd0b0897d92d
osinnworld <shemogata@gmail.com>
2023-07-13 12:24:04 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,19 @@
 #include <unistd.h>
 
-int main(void) {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(message) - 1;
-    ssize_t bytes_written = write(2, message, len);
+/**
+ * main - Program doesn/t use puts and print
+ *
+ * Return: 1 
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof(*message) - 1;
+	ssize_t bytes_written = write(2, message, len);
 
-    if (bytes_written != len) {
-        return 1;
-    }
+	if (bytes_written != len)
+		return (1);
 
-    return 0;
+	return (1);
 }
 
