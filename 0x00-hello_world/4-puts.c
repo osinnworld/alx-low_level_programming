72d0d2a659287754fcb25a79e9e900ded3594d66
osinnworld <shemogata@gmail.com>
2023-07-13 10:56:22 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h> 
-
 /**
  * main -prints out output using puts
  * 
  * description -print out
- */
-
+ *
+ * /
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle"); 
-	
 	return (0);
 } 
