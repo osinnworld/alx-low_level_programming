22b457dd84c57d0ecde3d33342304ef475dd358a
osinnworld <shemogata@gmail.com>
2023-07-13 12:46:29 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,14 +7,7 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - "
-			      "Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(*message) - 1;
-	ssize_t bytes_written = write(2, message, len);
-
-	if (bytes_written != len)
-		return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
