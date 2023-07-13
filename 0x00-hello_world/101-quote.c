9634deb6ef11129c3b3856059622959163ea121e
osinnworld <shemogata@gmail.com>
2023-07-13 12:35:56 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
 #include <unistd.h>
+#define STDERR 2
 
 /**
- * main - Entry point of the program
+ * main - No print and put
  *
- * Return: 1 (FAILURE)
+ * Return: Always 1
  */
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(*message) - 1;
-	ssize_t bytes_written = write(2, message, len);
+	ssize_t len = 59; 
+
+	ssize_t bytes_written = write(STDERR, message, len);
 
 	if (bytes_written != len)
 		return (1);
