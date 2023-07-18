281aeff73f15bea5a64cd7720e5c44014e46902d
osinnworld <shemogata@gmail.com>
2023-07-17 22:11:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _putchar - Custom putchar function
+ * @c: The character to be output
+ *
+ * Return: On success, 1. On error, -1.
+ */
+int _putchar(char c)
+{
+	return putchar(c);
+}
