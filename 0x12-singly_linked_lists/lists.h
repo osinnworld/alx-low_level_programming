cca3c4d4b08f6effe96ad61eb41e471de92b2e2c
osinnworld <shemogata@gmail.com>
2023-08-24 17:07:35 -0700

    more changes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-
 /**
  * struct list_s - singly l.l
- * @strg: strg
+ * @str: strg
  * @len: parameter
  * @next: parameter
  *
@@ -14,15 +12,15 @@
 typedef struct list_s
 {
 	unsigned int len;
-	char *strg
+	char *str
 	struct list_s *next;
 } list_t;
 
-int _putchar(char);
-size_t print_list(const list_t *);
-size_t list_len(const list_t *);
-list_t *add_node(list_t **, const char *);
-list_t add_node_end(list_t **, const char *);
-void free_list(list_t *);
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
