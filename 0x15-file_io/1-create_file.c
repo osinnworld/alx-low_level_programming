454ae54d7358b17cc15cddbbd65aa924900ebc3e
osinnworld <shemogata@gmail.com>
2023-09-04 13:02:12 -0700

    rev2-Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,20 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, wrt, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR, 600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	if (fd == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrt = write(fd, text_content, len);
+
+	if (fd == -1 || wrt == -1)
 		return (-1);
 
-	chmod(filename, 644);
-
-	if (text_content != NULL)
-		write(fd, text_content, strlen(text_content));
 	close(fd);
+
 	return (1);
 }
